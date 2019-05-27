a0d65fb897e35c23e43557ae6d79fa57b467a6a1
zhibi <1017866168@qq.com>
2019-05-27 09:04:04 +0800

    初始化

---
+++ src/utils/request.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import { MessageBox, Message } from 'element-ui'
+import store from '@/store'
+
+const service = axios.create({
+  // baseURL: process.env.VUE_APP_BASE_API,
+  baseURL: 'https://www.easy-mock.com/mock/5cdb6b1c196b3a1793f9fcad/jyk-admin',
+  withCredentials: true,
+  timeout: 10 * 1000
+})
+
+// response interceptor
+service.interceptors.response.use(
+  response => {
+    const res = response.data
+    if (!res.success) {
+      Message({
+        message: res.message || 'error',
+        type: 'error',
+        duration: 1.5 * 1000
+      })
+      // token已过期
+      if (res.code === 50014) {
+        MessageBox.confirm('Token 过期了，您可以取消继续留在该页面，或者重新登录', '确定登出', {
+          confirmButtonText: '重新登录',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          store.dispatch('user/resetToken').then(() => {
+            location.reload()
+          })
+        })
+      }
+      return Promise.reject(res)
+    } else {
+      return res
+    }
+  },
+  error => {
+    error.message = error.message === 'timeout of 10000ms exceeded' ? '连接服务器超时！' : error.message;
+    Message({
+      message: error.message,
+      type: 'error',
+      duration: 1.5 * 1000
+    })
+    return Promise.reject(error)
+  }
+)
+
+export default service
---
+++ src/views/login/index.vue
@@ -0,0 +1,212 @@
+<template>
+  <div class="login-container">
+    <div class="bg-animation">
+      <canvas id="cvs_bg" width="1443" height="596" />
+      <canvas id="cvs_key" />
+    </div>
+    <el-form
+      ref="loginForm"
+      class="login-form"
+      auto-complete="on"
+      status-icon
+      :model="loginForm"
+      :rules="loginRules"
+      label-position="left"
+    >
+      <h3 class="title">洁雅康后台管理</h3>
+      <el-form-item prop="username">
+        <span class="svg-container svg-container_login">
+          <i class="fa fa-user" />
+          <!--<svg-icon icon-class="user" />-->
+        </span>
+        <el-input
+          v-model.trim="loginForm.username"
+          name="username"
+          type="text"
+          spellcheck="false"
+          auto-complete="on"
+          maxlength="30"
+          clearable
+          placeholder="请输入账号"
+        />
+      </el-form-item>
+      <el-form-item prop="password">
+        <span class="svg-container svg-container_login">
+          <i class="fa fa-lock" />
+        </span>
+        <el-input
+          v-model.trim="loginForm.password"
+          name="password"
+          :type="pwdType"
+          spellcheck="false"
+          maxlength="30"
+          clearable
+          auto-complete="on"
+          placeholder="请输入密码"
+          @keyup.enter.native="handleLogin"
+        />
+        <span class="show-pwd" @click="showPwd"><i :class="['fa',eye]" /></span>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" class="login-button" :loading="loading" @click.native.prevent="handleLogin">
+          登录
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import { autoRun } from '@/utils/animation/keyDown'
+import { CanvasAnimate } from '@/utils/animation/canvasLogin'
+export default {
+  name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        username: process.env.VUE_APP_USERNAME,
+        password: process.env.VUE_APP_USER_PASSWORD
+      },
+      loginRules: {
+        username: [
+          { required: true, trigger: 'blur' },
+          { min: 5, max: 30, message: '长度在 5 到 30 个字符', trigger: 'change' }
+        ],
+        password: [
+          { required: true, trigger: 'blur' },
+          { min: 5, max: 30, message: '长度在 5 到 30 个字符', trigger: 'change' }
+        ]
+      },
+      loading: false,
+      eye: 'fa-eye-slash',
+      pwdType: 'password'
+    }
+  },
+  mounted() {
+    const canvasAnimate = new CanvasAnimate(document.getElementById('cvs_bg'),
+      { length: 100, clicked: true, moveon: true })
+    canvasAnimate.Run()
+    autoRun()
+  },
+  methods: {
+    showPwd() {
+      if (this.pwdType === 'password') {
+        this.pwdType = ''
+        this.eye = 'fa-eye'
+      } else {
+        this.pwdType = 'password'
+        this.eye = 'fa-eye-slash'
+      }
+    },
+    handleLogin() {
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          this.loading = true
+          this.$store.dispatch('user/login', this.loginForm).then(() => {
+            this.loading = false
+            this.$router.push('/index/list')
+            this.$message1000('登录成功', 'success')
+          }).catch(() => {
+            this.loading = false
+            // this.$router.push({ path: '/' }) //  跳过后台验证
+          })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+  $bg: #2d3a4b;
+  $light_gray: #eee;
+
+  /* reset element-ui css */
+  .login-container {
+    .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+      input {
+        background: transparent;
+        border: 0;
+        -webkit-appearance: none;
+        border-radius: 0;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        height: 47px;
+        &:-webkit-autofill {
+          -webkit-box-shadow: 0 0 0 1000px $bg inset !important;
+          -webkit-text-fill-color: #fff !important;
+        }
+      }
+    }
+    .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+  }
+
+</style>
+
+<style rel="stylesheet/scss" lang="scss">
+  $bg: #2d3a4b;
+  $dark_gray: #889aa4;
+  $light_gray: #eee;
+  .login-container {
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    background-color: $bg;
+    .login-form {
+      position: absolute;
+      left: 0;
+      right: 0;
+      width: 520px;
+      padding: 35px 35px 15px 35px;
+      margin: 90px auto;
+      z-index: 3;
+      .login-button{
+        width: 100%;
+      }
+    }
+    .svg-container {
+      padding: 6px 5px 6px 15px;
+      color: $dark_gray;
+      vertical-align: middle;
+      width: 30px;
+      display: inline-block;
+      &_login {
+        font-size: 20px;
+      }
+    }
+    .title {
+      user-select: none;
+      font-size: 26px;
+      font-weight: 400;
+      color: $light_gray;
+      margin: 0 auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+    .show-pwd {
+      position: absolute;
+      right: 10px;
+      top: 7px;
+      font-size: 16px;
+      color: $dark_gray;
+      cursor: pointer;
+      user-select: none;
+    }
+    #cvs_bg,#cvs_key{
+      position: absolute;
+    }
+    #cvs_bg{
+      z-index: 1
+    }
+  }
+</style>
