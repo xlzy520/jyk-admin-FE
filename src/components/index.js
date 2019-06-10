8678ff412888230662aac9fbebcec8c60200e7b2
zhibi <1017866168@qq.com>
2019-06-10 17:32:52 +0800

    table中value为0时的处理

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
-import XlTable from '@/components/XlTable'// svg component
-import XlForm from '@/components/XlForm'// svg component
+import XlTable from '@/components/XlTable'
 
 // register globally
 Vue.component('xl-table', XlTable)
-Vue.component('xl-form', XlForm)
