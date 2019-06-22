e9dc6a5fd01177274d27d05bdd1419784b1117f6
zhibi <1017866168@qq.com>
2019-06-22 22:44:59 +0800

    登录接口使用formData格式，getInfo使用post，logout、getInfo修改路径

--- src/api/user-admin.js
+++ src/api/user-admin.js
@@ -1,13 +1,33 @@
 import baseRequest from './base'
+import axios from 'axios'
 
 export default {
   login(data) {
-    return baseRequest('/user-admin/login', data)
+    return axios({
+      method: 'post',
+      url: '/market/login',
+      data: data,
+      // 转换数据的方法
+      transformRequest: [
+        function(data) {
+          let ret = ''
+          for (const it in data) {
+            ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+          }
+          ret = ret.substring(0, ret.length - 1)
+          return ret
+        }
+      ],
+      // 设置请求头
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
   },
   getInfo() {
-    return baseRequest('/user-admin/info', {}, 'get')
+    return baseRequest('/info')
   },
   logout() {
-    return baseRequest('/user-admin/logout')
+    return baseRequest('/logout')
   }
 }
