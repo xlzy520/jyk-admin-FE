50e42d93bc2feaff7f66065f80e5d3116def4002
zhibi <1017866168@qq.com>
2019-06-22 22:46:37 +0800

    修改baseURL，修改token过期处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
+import router from '@/router'
+import { resetRouter } from '@/router'
 
 const service = axios.create({
-  // baseURL: process.env.VUE_APP_BASE_API,
-  baseURL: 'https://www.easy-mock.com/mock/5cdb6b1c196b3a1793f9fcad/jyk-admin',
+  baseURL: process.env.VUE_APP_BASE_API,
+  // baseURL: 'https://www.easy-mock.com/mock/5cdb6b1c196b3a1793f9fcad/jyk-admin',
   withCredentials: true,
   timeout: 10 * 1000
 })
@@ -20,15 +22,15 @@ service.interceptors.response.use(
         duration: 1.5 * 1000
       })
       // token已过期
-      if (res.code === 50014) {
+      if (res.code === 1027) {
         MessageBox.confirm('Token 过期了，您可以取消继续留在该页面，或者重新登录', '确定登出', {
           confirmButtonText: '重新登录',
           cancelButtonText: '取消',
           type: 'warning'
         }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload()
-          })
+          resetRouter()
+          router.push('/login')
+          location.reload()
         })
       }
       return Promise.reject(res)
