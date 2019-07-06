8faa1c1e1ce564dce048718893ab32cb4c85759b
zhibi <1017866168@qq.com>
2019-07-06 21:13:28 +0800

    增加formdata请求格式

--- src/api/base.js
+++ src/api/base.js
@@ -5,5 +5,26 @@ export default function baseService(url, value, method = 'post', options = {}) {
     return request.post(url, value, options)
   } else if (method === 'get') {
     return request.get(url, { params: value }, options)
+  } else if (method === 'formdata') {
+    return request({
+      method: 'post',
+      url: url,
+      data: value,
+      // 转换数据的方法
+      transformRequest: [
+        function(data) {
+          let ret = ''
+          for (const it in data) {
+            ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+          }
+          ret = ret.substring(0, ret.length - 1)
+          return ret
+        }
+      ],
+      // 设置请求头
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
   }
 }
