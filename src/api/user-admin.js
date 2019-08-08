97e94460a3868b7b2670db42f7a60627e59e9927
zhibi <1017866168@qq.com>
2019-08-08 22:18:09 +0800

    优化登录接口请求

--- src/api/user-admin.js
+++ src/api/user-admin.js
@@ -1,28 +1,8 @@
 import baseRequest from './base'
-import axios from 'axios'
 
 export default {
   login(data) {
-    return axios({
-      method: 'post',
-      url: '/market/login',
-      data: data,
-      // 转换数据的方法
-      transformRequest: [
-        function(data) {
-          let ret = ''
-          for (const it in data) {
-            ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
-          }
-          ret = ret.substring(0, ret.length - 1)
-          return ret
-        }
-      ],
-      // 设置请求头
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    })
+    return baseRequest('/login', data, 'formdata')
   },
   getInfo() {
     return baseRequest('/info')
